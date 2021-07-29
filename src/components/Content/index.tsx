69431ebad643fd794a6eba835c19a977d8949b4d
wellington ribeiro <wellingtonvx@gmail.com>
2021-07-29 11:15:55 -0300

    add content

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,37 +1,78 @@
 import WommanCoddingImg from '../../assets/img/womman.png'
 
+import './style.scss'
 
 export function Content() {
   return (
     <div className="container">
       <header>
+
         <div>
           <h2>SifraPag</h2>
           <p>
-            Seja bem-vindo novamente. Estamos <br/>
+            Seja bem-vindo novamente. Estamos<br/>
             felizes em lhe ver :D.
           </p>
         </div>
+
         <img src={WommanCoddingImg} alt="Mulher no computador" />
+
       </header>
-      <main>
-      <div>
-        <header>
-          <p>Saldo disponível </p>
-          <button>
-            Trasferir
-          </button>
-        </header>
-          <strong>
-          {
-            new Intl.NumberFormat('pt-BR', {
-            style:"currency",
-            currency:"BRL"
-            }).format(685)
-          }
-          </strong>
-      </div>
+
+      <main className="content">
+        <section>
+          <div>
+            <header>
+              <p>Saldo disponível </p>
+            </header>
+            <main>
+              <strong>
+              {
+                new Intl.NumberFormat('pt-BR', {
+                style:"currency",
+                currency:"BRL"
+                }).format(685)
+              }
+              </strong>
+              <button>
+                Trasferir
+              </button>
+            </main>
+            <footer>
+              <span>*Valor sujeito a mudança até o final do dia</span>
+            </footer>
+          </div>
+
+          <div>
+            <header>
+              <p>Saldo A receber </p>
+            </header>
+            <main>
+              <strong>
+              {
+                new Intl.NumberFormat('pt-BR', {
+                style:"currency",
+                currency:"BRL"
+                }).format(20657)
+              }
+              </strong>
+
+              <button>
+                Visualizar detalhes
+              </button>
+            </main>
+            <footer>
+              <span>*Lançamentos previstos para entrar na sua conta</span>
+            </footer>
+
+          </div>
+        </section>
+        <section>
+          
+        </section>
+      
       </main>
+
     </div>
   )
 }
